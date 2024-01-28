c4d83f83d82e36e90df77fe652fc9deafed65479
Bhupender <bhupender.contact+github@gmail.com>
2024-01-28 12:13:43 +0530

    authrouter enabled

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,6 @@ import productRouter from "./productRouter.js";
 
 export const configureRoutes = (app) => {
   app.use("/", baseRouter);
-  // app.use("/auth", authRouter);
+  app.use("/auth", authRouter);
   // app.use("/products", productRouter);
 };
